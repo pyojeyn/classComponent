92e899eb2196f6432696c565b93d402f18b912f1
pyojeyn <jeynpyo@gmail.com>
2023-06-21 21:30:17 +0900

    사용중인 수명 주기 방법

--- src/components/User.js
+++ src/components/User.js
@@ -5,6 +5,10 @@ import classes from './User.module.css';
 
 // 클래스 기반 컴포넌트는 함수형 컴포넌트와 작업이 가능하다.
 class User extends Component{
+  componentWillUnmount(){
+    console.log("user will unmount");
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
